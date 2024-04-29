6abd7d31e7feb65e86a8442c77235eacd1b94932
joypaul1 <jyla5102555@gmail.com>
2024-04-29 12:52:38 +0600

    s

--- home/dashboard_adm.php
+++ home/dashboard_adm.php
@@ -110,7 +110,62 @@ while ($data = oci_fetch_assoc($salesSQL)) { // Fetch each row as an associative
                                 background-color: #1E3FB4;
                             }
                         </style>
-                        <form action="<?= $basePath ?>/report_panel/view/summary.php" method="POST" class="">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label for="title">Select Brand:</label>
+                                        <select required name="product_brand" id="product_brand" class="form-control">
+                                            <?php
+                                            if ($emp_session_band == "MM") {
+                                                renderOption('Mahindra', 'Mahindra');
+                                                renderOption('Eicher', 'Eicher');
+                                                renderOption('Dongfeng', 'DONGFENG');
+                                            } elseif ($emp_session_band == "EICHER") {
+                                                renderOption('Eicher', 'Eicher');
+                                                renderOption('Mahindra', 'Mahindra');
+                                                renderOption('Dongfeng', 'DONGFENG');
+                                            }
+
+                                            function renderOption($label, $value)
+                                            {
+                                                $selected = isset($_POST['product_brand']) && $_POST['product_brand'] == $value ? 'selected="selected"' : '';
+                                                echo '<option value="' . $value . '" ' . $selected . '>' . $label . '</option>';
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <label for="title">Entry From: <?php echo $emp_session_band; ?></label>
+                                    <div class="input-group">
+                                        <input required="" class="form-control" type='date' name='start_date' value='<?php echo isset($_POST['start_date']) ? date('Y-m-d', strtotime($_POST['start_date'])) : ''; ?>' />
+
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <label for="title">Entry To:</label>
+                                    <div class="input-group">
+                                        <input required="" class="form-control" type='date' name='end_date' value='<?php echo isset($_POST['end_date']) ? date('Y-m-d', strtotime($_POST['end_date'])) : ''; ?>' />
+                                    </div>
+                                </div>
+                                <!-- <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label for="title">Select Product:</label>
+                                        <select name="product_type" id="product_type" class="form-control"></select>
+                                    </div>
+                                </div> -->
+                            </div>
+                            <div class="d-flex justify-content-end">
+                                <div class=" text-end">
+                                    <button class=" btn btn-primary" type="submit" value="Load Data">
+                                        Load Data
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+                        <!-- <form action="<?= $basePath ?>/report_panel/view/summary.php" method="POST" class="">
                             <div class="row ">
                                 <input hidden name="product_type" value="">
                                 <div class="col-sm-3">
@@ -147,7 +202,7 @@ while ($data = oci_fetch_assoc($salesSQL)) { // Fetch each row as an associative
                                     <button type="submit" class="btn btn-primary btn-md mb-2">Generate Report<i class="las la-angle-right ms-3 scale5"></i></button>
                                 </div>
 
-                        </form>
+                        </form> -->
                     </div>
 
                 </div>
