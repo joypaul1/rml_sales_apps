e56b07a16c08c541de97ff30ddbf4405e0ca2f5a
joypaul1 <jyla5102555@gmail.com>
2024-04-29 13:01:02 +0600

    card-body

--- home/dashboard_adm.php
+++ home/dashboard_adm.php
@@ -109,28 +109,28 @@ while ($data = oci_fetch_assoc($salesSQL)) { // Fetch each row as an associative
                             background-color: #1E3FB4;
                         }
                     </style>
-                    <!-- <div class="col-xl-7 col-xxl-12 mt-3"> -->
+                    <div class="card-body">
 
-                    <form action="<?= $basePath ?>/report_panel/view/summary.php" method="POST" class="">
-                        <div class="row ">
-                            <input hidden name="product_type" value="">
-                            <div class="col-sm-3">
-                                <select required name="product_brand" id="product_brand" class="form-control col-md-2">
+                        <form action="<?= $basePath ?>/report_panel/view/summary.php" method="POST" class="">
+                            <div class="row ">
+                                <input hidden name="product_type" value="">
+                                <div class="col-sm-3">
+                                    <select required name="product_brand" id="product_brand" class="form-control col-md-2">
 
-                                </select>
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="text" id="start_date" name="start_date" placeholder="EX : 1-DEC-2024" class="from-control mb-2" value="<?= date('01-F-Y') ?>" autocomplete="off" required style="height: 37px;border-radius: 10px;border: 1px solid #1E3FB4;text-align: center;color: #1E3FB4;">
-                            </div>
-                            <div class="col-sm-3">
-                                <input type="text" id="end_date" name="end_date" placeholder="EX : 31-DEC-2024" class="from-control  mb-2" value="<?= date('t-F-Y') ?>" autocomplete="off" required style="height: 37px;border-radius: 10px;border: 1px solid #1E3FB4;text-align: center;color: #1E3FB4;" />
-                            </div>
-                            <div class="col-sm-3">
-                                <button type="submit" class="btn btn-primary btn-md mb-2">Generate Report<i class="las la-angle-right ms-3 scale5"></i></button>
-                            </div>
+                                    </select>
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="text" id="start_date" name="start_date" placeholder="EX : 1-DEC-2024" class="from-control mb-2" value="<?= date('01-F-Y') ?>" autocomplete="off" required style="height: 37px;border-radius: 10px;border: 1px solid #1E3FB4;text-align: center;color: #1E3FB4;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="text" id="end_date" name="end_date" placeholder="EX : 31-DEC-2024" class="from-control  mb-2" value="<?= date('t-F-Y') ?>" autocomplete="off" required style="height: 37px;border-radius: 10px;border: 1px solid #1E3FB4;text-align: center;color: #1E3FB4;" />
+                                </div>
+                                <div class="col-sm-3">
+                                    <button type="submit" class="btn btn-primary btn-md mb-2">Generate Report<i class="las la-angle-right ms-3 scale5"></i></button>
+                                </div>
 
-                    </form>
-                    <!-- </div> -->
+                        </form>
+                    </div>
                     <div class="card-body">
                         <form action="" method="post">
                             <div class="row">
